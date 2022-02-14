8e5e83cebae051717149b9baf1d805dac6a4d687
unknown <steinway.nathan@gmail.com>
2022-02-13 21:33:26 -0600

    repaired connection between environment variables and mysqlcreateconnection

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
+require('dotenv').config();
 
-const db = mysql.createConnection(
+const connection = mysql.createConnection(
     {
       host: 'localhost', 
       user: process.env.DB_USER,
-      password: process.env.DB_PW,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
 
