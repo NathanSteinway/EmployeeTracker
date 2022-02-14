f7467559eef4498313d7654d2e2ac8ca7e009e8e
unknown <steinway.nathan@gmail.com>
2022-02-13 21:00:35 -0600

    added function to showOptions to user then launch given functions as a response

--- index.js
+++ index.js
@@ -15,5 +15,76 @@ const db = mysql.createConnection(
 
 connection.connect(function(err){
   if (err) throw err;
-  options();
+  showOptions();
 })
+
+function showOptions() {
+  inquirer
+    .prompt({
+      name: 'list',
+      type: 'option',
+      message: 'You are now viewing our employee database. What would you like to do next?',
+      choices: [
+        'View all employees',
+        'View all departments',
+        'View all roles',
+        'Add an employee',
+        'Add a department',
+        'Add a role',
+        'Update employee role',
+        ]
+      }).then(function (answer) {
+          switch (answer.action) {
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Update employee role':
+                updateRole();
+                break;
+          }
+    });
+};
+
+function viewEmployees() {
+
+}
+
+function viewDepartments() {
+
+}
+
+function viewRoles() {
+
+}
+
+function addEmployee() {
+
+}
+
+function addDepartment() {
+
+}
+
+function addRole() {
+
+}
+
+function updateRole() {
+
+}
+
